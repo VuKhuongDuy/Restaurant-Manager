ea0b744182435634d1e91822a5f95e7ac9a6606a
VukhuongDuy <vukhuongduy23@gmail.com>
2019-04-10 16:38:59 +0700

    update login

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
-import MyRouter from '../MyRouter';
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
-import Header from '../header/header';
+import MyRouter from '../myrouter';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-class login extends Component {
+class Login extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            authenticated: "true",
-            reset:1
-        }
-
-        this.account = "";
-        this.password = "";
-    }
-
-    componentWillMount(){
         this.users = [];
         this.loadData();
-        console.log(this.users);
+        this.account = "";
+        this.password = "";
+
+        this.state = {
+            reset: 1,
+            redirectToReferrer: "false"
+        }
     }
 
-    componentDidMount() {
-        this.handleClick();
-    }
-
-    loadData(){
-        const data = {
-            name: 1
-        };
+    loadData() {
         const url = "http://localhost:3001/";
-        const response = fetch(url, {
+        fetch(url, {
             method: 'GET',
             mode: "cors",
             cache: "no-cache",
@@ -44,58 +32,59 @@ class login extends Component {
         }).then(response => response.json())
             .then(data => {
                 data.map((value, key) => {
-                    console.log(value);
                     this.users.push(value);
                 })
-            })
+            });
     }
 
-    reset(){
+    reset() {
         this.setState({
-            reset:3
+            reset: 3
         });
     }
 
-
-    handleClick() {
-        let linkGo = document.getElementsByClassName('btn-loginIn');
-        linkGo[0].addEventListener('click', function (event) {
-            let count = 0;
-            const input = document.getElementsByClassName('txtLogin');
-
-            this.users.map((value, key) => {
-                if (input[0].value === value.user_account && input[1].value === value.user_password) {
-                    this.account = value.user_account;
-                    this.password = value.user_password;
-                    count = 1;
-                    return;
-                }
-            })
-            if (count == 0) {
-                event.preventDefault();
-                alert('Tài khoản hoặc mật khẩu không hợp lệ');
+    login() {
+        let count = 0;
+        const input = document.getElementsByClassName('txtLogin');
+        this.users.map((value, key) => {
+            if (input[0].value === value.user_account && input[1].value === value.user_password) {
+                this.account = value.user_account;
+                this.password = value.user_password;
+                count = 1;
+                return;
             }
-
-        }.bind(this));
+        })
+        if (count === 0) {
+            alert('Tài khoản hoặc mật khẩu không hợp lệ');
+        } else {
+            fakeAuth.authenticate(() => {
+                this.setState(() => ({
+                    redirectToReferrer: "true"
+                }))
+            });
+        }
     }
 
     clickLoginUp() {
-        let txtNewUser = this.refs.txtNewUser;
-        let txtNewPassword = this.refs.txtNewPassword;
+        let txtNewUser = this.refs.txtNewUser.value.trim();
+        let txtNewPassword = this.refs.txtNewPassword.value.trim();
         let accountExit = 'true';
-        console.log(this.users);
 
-        for(let i=0;i< this.users.length;i++){
-            if (this.users[i].user_account === txtNewUser.value) {
-                alert('Tài khoản ' + txtNewUser.value + " đã tồn tại");
+        if(txtNewUser.length === 0 || txtNewPassword.length === 0){
+            accountExit = 'false';
+            alert('Hãy điền đầy đủ thông tin');
+        }
+        else for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].user_account === txtNewUser) {
+                alert('Tài khoản ' + txtNewUser + " đã tồn tại");
                 accountExit = 'false';
-                i = this.users.length+1;
+                i = this.users.length + 1;
             }
         }
 
         if (accountExit === 'true') {
             alert('Tạo tài khoản thành công!Bạn đã có thể sử dụng tài khoản vừa tạo');
-            const response = fetch("http://localhost:3001/", {
+            fetch("http://localhost:3001/", {
                 method: 'POST',
                 headers: {
                     "Content-Type": "application/json",
@@ -103,47 +92,95 @@ class login extends Component {
                 },
                 body: JSON.stringify({
                     user: {
-                        firstParam: txtNewUser.value,
-                        secondParam: txtNewPassword.value
+                        firstParam: txtNewUser,
+                        secondParam: txtNewPassword
                     }
                 })
-            })
+            });
             this.setState({
-                reset:3
+                reset: 3
             });
         }
     }
 
     render() {
-        return (
-            <div id="Login">
-                <div className="Login-BackGround">
-                    <div className="background-up"></div>
-                    <div className="background-down">
-                        <img className="img1" src="./img/cheef1.jpg" alt="cheef1" />
-                        <img className="img2" src="./img/cheef2.jpg" alt="cheef2" />
+        const { from } = this.props.location.state || { from: { pathname: '/dashboard' } }
+        const { redirectToReferrer } = this.state
+
+        if (redirectToReferrer === "true") {
+            return <Redirect to="/dashboard"/>
+        }
+        else return (
+                <div id="Login">
+                    <div className="Login-BackGround">
+                        <div className="background-up"></div>
+                        <div className="background-down">
+                            <img className="img1" src="../img/cheef1.jpg" alt="cheef1" />
+                            <img className="img2" src="../img/cheef2.jpg" alt="cheef2" />
+                        </div>
+                    </div>
+                    <div className="Login-Form" ref="login_form">
+                        <div className="title" ref="title_login">Login in</div>
+                        <input type="text" className="txtLogin txtUser" placeholder="User" autoFocus />
+                        <input type="password" className="txtLogin txtPassword" placeholder="Password" />
+                        <div className="forgot-password">
+                            <i className="fa fa-question-circle" style={{ marginRight: "7px" }} aria-hidden="true"></i>
+                            <a href="https://www.google.com/search?ei=2LGUXNiYA87W-Qb4kJm4Bg&q=how+to+find+my+password+on+the+web+of+V%C5%A9+Duy&oq=how+to+find+my+password+on+the+web+of+V%C5%A9+Duy&gs_l=psy-ab.3..33i160.1613.11080..11954...5.0..0.194.3729.1j25......0....1..gws-wiz.......0i71j0i203j0i22i30j0i22i10i30j33i22i29i30j33i21.eNaSd2yZGGA">Forgot Password</a>
+                        </div>
+                        <button className="btn btn-loginIn" onClick={this.login.bind(this)}>Go</button>
+                        {/* <div style={{ width: "100%", height: "1px", backgroundColor: "#4298f4", marginLeft: "0px", marginTop: "15px"}}></div> */}
+                        <hr/>
+                        <div className="form-login-up" ref="loginup_form">
+                            <div className="input-container">
+                                <i className="fa fa-user icon"></i>
+                                <input className="input-field" ref="txtNewUser" type="text" placeholder="Username" name="usrnm" />
+                            </div>
+                            <div className="input-container">
+                                <i className="fa fa-envelope icon"></i>
+                                <input className="input-field"  type="text" placeholder="Email" name="email" />
+                            </div>
+
+                            <div className="input-container">
+                                <i className="fa fa-key icon"></i>
+                                <input className="input-field" ref="txtNewPassword" type="password" placeholder="Password" name="psw" />
+                            </div>
+                        </div>
+                        <button className="btn btn-loginUp" onClick={this.clickLoginUp.bind(this)}>Log up</button>
                     </div>
                 </div>
-                <div className="Login-Form" ref="login_form">
-                    <div className="title" ref="title_login">Login in</div>
-                    <input type="text" className="txtLogin txtUser" placeholder="User" autoFocus />
-                    <input type="password" className="txtLogin txtPassword" placeholder="Password" />
-                    <div className="forgot-password">
-                        <i className="fa fa-question-circle" style={{ marginRight: "7px" }} aria-hidden="true"></i>
-                        <a href="https://www.google.com/search?ei=2LGUXNiYA87W-Qb4kJm4Bg&q=how+to+find+my+password+on+the+web+of+V%C5%A9+Duy&oq=how+to+find+my+password+on+the+web+of+V%C5%A9+Duy&gs_l=psy-ab.3..33i160.1613.11080..11954...5.0..0.194.3729.1j25......0....1..gws-wiz.......0i71j0i203j0i22i30j0i22i10i30j33i22i29i30j33i21.eNaSd2yZGGA">Forgot Password</a>
-                    </div>
-                    <NavLink to="/dashboard" className="btn btn-loginIn">Go</NavLink>
-                    <div className="form-login-up" ref="loginup_form">
-                        <hr />
-                        <input type="text" ref="txtNewUser" className="txtLoginUp newUser" placeholder="User" />
-                        <input type="password" ref="txtNewPassword" className="txtLoginUp newPassword" placeholder="Password" />
-                        <input type="text" ref="txtNewEmail" className="txtLoginUp newEmail" placeholder="abcxyz@gjgjg.ads" />
-                    </div>
-                    <button className="btn btn-loginUp" onClick={this.clickLoginUp.bind(this)}>Login up</button>
-                </div>
-            </div>
         );
     }
 }
 
-export default login;
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(() => {
+            cb();
+        }, 100)
+    },
+    signout(cb) {
+        this.isAuthenticated = false
+        setTimeout(cb, 100)
+    }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props => (
+        fakeAuth.isAuthenticated === true
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/' }} />
+    ))}/>
+)
+
+export default function MyRoute() {
+    return (
+        <Router>
+            <div>
+                <Route path="/" component={Login} />
+                <PrivateRoute path='/dashboard' component={MyRouter} />
+            </div>
+        </Router>
+    )
+}
